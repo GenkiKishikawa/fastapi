9e61194ca3ad1ca2c606dd591c12d25a813e2b6d
KishikawaGenki <kishikawa@vic.co.jp>
2024-03-19 18:51:54 +0900

    Add config file

---
+++ api/core/config.py
@@ -0,0 +1,41 @@
+from pydantic import (
+    PostgresDsn
+)
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+	model_config = SettingsConfigDict(
+		env_file=".env", env_ignore_empty=True, extra="ignore"
+	)
+ 
+	POSTGRES_SERVER: str
+	POSTGRES_PORT: int = 5432
+	POSTGRES_USER: str
+	POSTGRES_PASSWORD: str
+	POSTGRES_DB: str = "demo"
+	
+	@property
+	def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+		return MultiHostUrl.build(
+			scheme="postgresql+psycopg2",
+			username=self.POSTGRES_USER,
+			password=self.POSTGRES_PASSWORD,
+			host=self.POSTGRES_SERVER,
+			port=self.POSTGRES_PORT,
+			path=self.POSTGRES_DB
+		)
+  
+	@property
+	def ASYNC_SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+		return MultiHostUrl.build(
+			scheme="postgresql+asyncpg",
+			username=self.POSTGRES_USER,
+			password=self.POSTGRES_PASSWORD,
+			host=self.POSTGRES_SERVER,
+			port=self.POSTGRES_PORT,
+			path=self.POSTGRES_DB
+		)
+
+settings = Settings()
