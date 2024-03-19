560db5d196b6e78a720e75072e37bb73bcd976b5
KishikawaGenki <kishikawa@vic.co.jp>
2024-03-19 19:10:35 +0900

    Move db file to api/core and apply it to related files

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from api.db import get_db, Base
+from api.core.db import get_db, Base
 from api.main import app
 
 import starlette.status
