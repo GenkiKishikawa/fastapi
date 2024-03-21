02a2ade3dd46281ce9a42a1df6c3eeec549e50c0
KishikawaGenki <kishikawa@vic.co.jp>
2024-03-21 13:53:11 +0900

    Add custom exception handling

---
+++ api/exceptions/core.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from fastapi import HTTPException, status
+
+
+class APIException(HTTPException):
+    
+    default_status_code = status.HTTP_400_BAD_REQUEST
+    
+    def __init__(
+        self,
+        error: Any,
+        status_code: int = default_status_code,
+        headers: dict[str, Any] | None = None
+    ) -> None:
+        self.headers = headers
+        try:
+            error_obj = error()
+        except Exception:
+            error_obj = error
+
+        try:
+            message = error_obj.text.format(error_obj.param)
+        except Exception:
+            message = error_obj.text
+
+        try:
+            status_code = error_obj.status_code
+        except Exception:
+            status_code = status_code
+
+        self.detail = {"error_code": str(error_obj), "error_msg": message}
+        print(self.detail)
+        super().__init__(status_code, detail=self.detail)
