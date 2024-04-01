c206880d4d58dc3f328d418a492abeefe0d6f471
KishikawaGenki <kishikawa@vic.co.jp>
2024-04-01 09:51:20 +0900

    Add CORS configuration

--- api/main.py
+++ api/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from api.core.config import settings
 from api.routers import task, done
 
 
 app = FastAPI()
+
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+		allow_origins=[
+			str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+		],
+		allow_credentials=True,
+		allow_methods=["*"],
+		allow_headers=["*"],
+	)
+
+
 app.include_router(task.router)
 app.include_router(done.router)
